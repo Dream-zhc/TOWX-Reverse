ec448dc36825918418ccca8533e7f855bd4c0602
Dream-猫曦 <83204117+Dream-zhc@users.noreply.github.com>
2026-08-14 17:01:28 +0800

    fix: gate avatar module by underlying host app

--- SpringBoardDataBridge/TOWXV11Gate.h
+++ SpringBoardDataBridge/TOWXV11Gate.h
@@ -6,6 +6,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 FOUNDATION_EXPORT BOOL TOWXV11ShouldShowAvatarModule(BOOL sessionVisible,
                                                      NSString * _Nullable sessionBundleID,
+                                                     NSString * _Nullable hostBundleID,
                                                      BOOL weChatActive,
                                                      NSUInteger avatarCount,
                                                      const char * _Nullable * _Nullable reasonOut);
